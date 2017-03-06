f9ec048336aca7eb2aa669695b1fe2c22c5883aa
yugalpsd1 <yugalpsd1>
2017-03-06 12:25:42 +0630

    added task statistics and progress bar for tasks.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ window.onload = function() {
             }
         },
         props: ['task'],
-        template: '<div><span :class="{ taskDone: task.done }" v-on:click="clicked" v-show="!inEditMode">{{task.name}}</span><textarea  style="max-width=40px;" height="600" autofocus class="form-control" v-on:blur="saved" v-on:keyup.enter="saved" v-model="task.name" v-show="inEditMode"/></div>',
+        template: '<div><span :class="{ taskDone: task.done }" v-on:click="clicked" v-show="!inEditMode">{{task.name}}</span><textarea  style="max-width=40px;" height="600"autofocus class="form-control" v-on:blur="saved" v-on:keyup.enter="saved" v-model="task.name" v-show="inEditMode"/></div>',
         methods: {
             clicked: function() {
                 this.inEditMode = true;
@@ -27,54 +27,11 @@ window.onload = function() {
         el: '#app',
         data: {
             displayTasks: true,
+            displayProgressBar: true,
             displayAddTasks: true,
-            tasks: [{
-                    name: "add edit feature",
-                    done: true
-                },
-                {
-                    name: "change ui",
-                    done: true
-                },
-                {
-                    name: "add bootstrap",
-                    done: true
-                },
-
-                {
-                    name: "fix bugs",
-                    done: true
-                },
-                {
-                    name: "add task status",
-                    done: true
-                },
-                {
-                    name: "add dragula.js",
-                    done: false
-                },
-				{
-                    name: "implement dragula js",
-                    done: false
-                },
-				{
-                    name: "remove dragula js",
-                    done: false
-                },
-				{
-                    name: "add sortable js",
-                    done: false
-                },
-				{
-                    name: "implement sortable js",
-                    done: false
-                },
-				{
-                    name: "change ui again",
-                    done: false
-                },
-
-            ]
+            displayTaskStatistics: true,
+            displayedTasksStat: 'totalTasks',
+            tasks: []
         },
         methods: {
 
@@ -90,9 +47,56 @@ window.onload = function() {
             },
             deleteTask: function(task) {
                 this.tasks.splice(this.tasks.indexOf(task), 1);
+            },
+            changeTotalTasks: function() {
+                this.displayedTasksStat = 'totalTasks';
+            },
+            changeLeftToDo: function() {
+                this.displayedTasksStat = 'leftToDo';
+            },
+            changeCheckMarked: function() {
+                this.displayedTasksStat = 'checkMarked';
             }
 
+        },
+        computed: {
+            checkMarkedTasks: function() {
+                let count = 0;
+                for (let i = 0; i < this.tasks.length; ++i) {
+                    if (this.tasks[i].done == true) {
+                        count++;
+                    }
+                }
+                return count;
+            },
+            leftToDo: function() {
+                return this.tasks.length - this.checkMarkedTasks;
+            },
+            displayedTasksStatView: function() {
+                if (this.displayedTasksStat == 'totalTasks') {
+                    return 'Total Tasks: ' + this.tasks.length;
+                } else if (this.displayedTasksStat == 'leftToDo') {
+                    return 'Tasks Left: ' + this.leftToDo;
+                } else if (this.displayedTasksStat == 'checkMarked') {
+                    return 'Check Marked Tasks: ' + this.checkMarkedTasks;
+                } else if (this.displayedTasksStat == 'deletedTasks') {
+                    return 'Deleted Tasks: ' + this.deletedTasks;
+                }
+            },
+            manageable: function() {
+                if (this.leftToDo < 5) {
+                    return 'green';
+                } else {
+                    return 'red';
+                }
+            },
+            percentageOfTasksCompleted: function() {
+                if (this.tasks.length == 0) {
+                    return 0;
+                } else {
+                    return (this.checkMarkedTasks / this.tasks.length) * 100;
+                }
+            }
         }
-       
     });
 }
